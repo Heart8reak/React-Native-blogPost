f989314c259f8681f6156ce9a4c29893cbf6fe5f
Carlos Baez <c.baez1223@yahoo.com>
2021-02-09 18:57:26 -0500

    added the create screen with blogpostform

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react' 
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 
-const BlogPostForm = () => {
+const BlogPostForm = ({ onSubmit }) => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
 
@@ -21,6 +21,7 @@ const BlogPostForm = () => {
             />
             <Button 
             title='Save Blog Post'
+            onPress={() => onSubmit(title, content)}
             />
         </View>
     )
